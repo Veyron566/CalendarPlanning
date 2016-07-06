ba813d6e48bd9b6ee2d837d7478d6d3fa1389e01
= <=>
2016-07-06 03:06:36 +0300

    Constructor of graph from rough data added

--- Main.py
+++ Main.py
@@ -1,23 +1,16 @@
 from src.node import *
 from src.graph import *
 # Some graph definition end output example
-init = Node("init")
-end = Node ("term")
-a = Node(1)
-b = Node(2)
-c = Node(3)
-d = Node(4)
-e = Node(5)
-g = Graph(init,end)
-map (g.add_node ,[a,b,c,d,e] )
-Node.add_arrow(init,a,0)
-Node.add_arrow(a,b,10)
-Node.add_arrow(b,c,12)
-Node.add_arrow(b,d,7)
-Node.add_arrow(d,e,5)
-Node.add_arrow(c,e,6)
-Node.add_arrow(e,end,0)
+Data = [('A',[], 5,5),
+        ('B',['A'],4,4),
+        ('C', ['A'],3,3),
+        ('D',['B','C'],2,2),
+        ('E',['D'],1,1)]
+
+g = Graph(Data)
 
 g.stright_progn(g.Begin)
 g.backward_progn(g.End)
-g.to_file('mygraph.gv')
+
+g.to_file("mygraph.gv").view()
+
--- src/graph.py
+++ src/graph.py
@@ -1,28 +1,12 @@
 from graphviz import Digraph
-# Node has indexes L , F and two dicts of in and out node
-class Node:    
-    def __getitem__(self, key):
-        return self.Arrows[key]
-    def __init__(self,name):
-        self.L = ()
-        self.F = ()
-        self.Out = {}
-        self.In = {}
-        self.Name = name
-    def add_arrow(b_node,e_node,weight):
-        b_node.Out[e_node] = weight
-        e_node.In[b_node] = -weight
-
+from node import Node
 # Class contains:
 # List of nodes
 # Initial and terminal mode
 #
 # Description of some methods that could be useful
 class Graph:
-    def __init__(self,b,e):
-        self.Begin = b
-        self.End = e
-        self.nodes = {e,b}        
+    Arrows= {}
     def add_node (self,node):
         self.nodes.add(node)
     def stright_progn(self,initial_node):
@@ -44,12 +28,24 @@ class Graph:
         def arcs_for_node(n):
             return map (lambda x:(n, x, n.Out[x]), n.Out)    
         return map (arcs_for_node,self.nodes)
-   
+    def simple_to_file (self,d):
+        f = Digraph('D',
+                    filename = d) 
+        
+        for i in self.nodes:
+            f.node(str (i),str (i.Name))
+                   
+      #to_graphviz_record(i.Name,i.L,i.F,i.F-i.L)             
+        for i in sum (self.get_arcs(), []):
+            f.edge(str ((i[0])),
+                   str ((i[1])), 
+                   label = str ( i[2]))
+        return f
     def to_file (self,d):
         f = Digraph('D',
                     filename = d, 
                     node_attr = {'shape': 'record'} )
-
+        
         for i in self.nodes:
             f.node(str (i),(str (to_graphviz_record (i.Name,i.L,i.F,i.F-i.L))))
                    
@@ -59,7 +55,60 @@ class Graph:
                    str ((i[1])), 
                    label = str ( i[2]))
         return f
-
+# Creating Graph From Data, where Data is List[(char, [Previous],time)]
+    def __init__(self,Data):
+        dat = ()
+        nd = ()
+        i = 1
+        j = 0
+        self.nodes = set()
+        while  Data != []:
+            dat = Data[j] # Data[j] is tuple like this ('A', ['C','D'], 3,5)
+            prev = dat[1]
+            name = dat[0]
+            cost = dat[3]
+            dur = dat [2]
+            j =  j + 1
+            if (prev == []):
+                j = 0
+                nd = Node(0)
+                self.Begin = nd
+                self.Arrows[name] = (nd,(),dur)
+                Data.remove(dat)
+                continue
+            if(all (map (self.Arrows.has_key, prev))):
+                j = 0
+                Data.remove(dat)
+                if (self.Arrows[prev[0]][1] == ()):
+                    nd = Node(i)
+                    i = i+1
+                else:
+                    nd = self.Arrows[prev[0]][1]
+                
+                self.Arrows[name] = (nd,(),dur)
+                self.Arrows[prev[0]] =(self.Arrows[prev[0]][0],
+                                       nd,
+                                       self.Arrows[prev[0]][2])
+                for n in prev[1:]:
+                    
+                    fict = Node (i)
+                    self.add_node(fict)
+                    i =  i + 1 
+                    self.Arrows[n] = (self.Arrows[n][0],
+                                       fict,
+                                       self.Arrows[n][2])
+                    Node.add_arrow (fict,nd,0)
+                   
+        term = Node(i)
+        self.Arrows[name] = (self.Arrows[name][0],
+                               term,
+                               self.Arrows[name][2])
+        self.End = term
+        self.nodes.add(self.Begin)
+        self.nodes.add(self.End)
+        for a in self.Arrows.values():
+            self.nodes.add(a[0])
+            Node.add_arrow(a[0],a[1],a[2])
 # Some procedures 
 def mymax (a,b):
     if a == ():
